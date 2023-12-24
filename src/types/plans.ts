ad3efe33d9d117d9febb373dfe44c19ebe1eda93
goodeffort <lukehsurvey@gmail.com>
2023-12-23 21:09:58 -0500

    changed module types to classes

--- src/types/plans.ts
+++ src/types/plans.ts
@@ -1,18 +1,53 @@
-type OperatorPlans = {
-    operatorId: string;
+class SkillMasteries {
+    skill1: number;
+    skill2: number;
+    skill3: number;
 
+    constructor() {
+        this.skill1 = 0;
+        this.skill2 = 0;
+        this.skill3 = 0;
+    }
+}
+
+class ModuleLevels {
+    x: number;
+    y: number;
+    z: number;
+
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+}
+
+class OperatorPlans {
     currentLevel: number;
     currentElite: number;
     currentSkillLevels: number;
-    currentSkillMasteries: { skill1: number; skill2: number; skill3: number; };
-    currentModules: { x: number; y: number; z: number; };
+    currentSkillMasteries: SkillMasteries;
+    currentModules: ModuleLevels;
 
     targetLevel: number;
     targetElite: number;
     targetSkillLevels: number;
-    targetSkillMasteries: { skill1: number; skill2: number; skill3: number; };
-    targetModules: { x: number; y: number; z: number; };
+    targetSkillMasteries: SkillMasteries;
+    targetModules: ModuleLevels;
 
+    constructor() {
+        this.currentLevel = 1;
+        this.currentElite = 0;
+        this.currentSkillLevels = 1;
+        this.currentSkillMasteries = new SkillMasteries();
+        this.currentModules = new ModuleLevels();
+
+        this.targetLevel = 1;
+        this.targetElite = 0;
+        this.targetSkillLevels = 1;
+        this.targetSkillMasteries = new SkillMasteries();
+        this.targetModules = new ModuleLevels();
+    }
 }
 
-export type { OperatorPlans }
+export { OperatorPlans }
