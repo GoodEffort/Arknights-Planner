dee1373d96640e05220448c103103f34b2977e2a
goodeffort <lukehsurvey@gmail.com>
2023-12-23 21:15:18 -0500

    changed characters to operators

--- src/components/SelectedOperators.vue
+++ src/components/SelectedOperators.vue
@@ -4,15 +4,27 @@ import { usePlannerStore } from '../store/planner-store';
 import { storeToRefs } from 'pinia';
 import OperatorControls from './OperatorControls.vue';
 
-const { selectedCharacters } = storeToRefs(usePlannerStore());
+const { selectedOperators } = storeToRefs(usePlannerStore());
 </script>
 
 <template>
     <PlannerSection title="Selected Operators" :initial-state="true">
         <div class="container">
-            <div class="row" v-for="character in selectedCharacters" :key="character.id">
-                <OperatorControls :character="character" />
+            <div class="row mb-3 py-3" v-for="character in selectedOperators" :key="character.operator.id">
+                <OperatorControls :selected-operator="character" />
             </div>
         </div>
     </PlannerSection>
 </template>
+
+<style scoped>
+html.dark .row {
+    background-color: #050505;
+}
+
+.row {
+    background-color: #F0F0F0;
+    border-radius: 5px;
+    border: 1px solid #1a1a1a;
+}
+</style>
