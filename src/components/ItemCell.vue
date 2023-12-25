f5a0a583c3e7ced3363a65d787c5e7708c88a4fd
goodeffort <lukehsurvey@gmail.com>
2023-12-25 00:25:37 -0500

    added itemcell component

---
+++ src/components/ItemCell.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { usePlannerStore } from '../store/planner-store';
+import { Item } from '../types/item';
+
+defineProps<{
+    item: Item;
+    count: number;
+}>();
+
+const { getItemImageLink } = usePlannerStore();
+</script>
+
+<template>
+    <div class="col">
+        <div>
+            <img :src="getItemImageLink(item)" :alt="item.name" class="img-thumbnail item-image" />
+        </div>
+        <div class="name">
+            {{ item.name }}
+        </div>
+        <div class="count">
+            {{ count }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.item-image {
+    max-width: 50px;
+    max-height: 50px;
+    object-fit: contain;
+}
+</style>
