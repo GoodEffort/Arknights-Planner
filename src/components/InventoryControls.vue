fe628d21be58c034bc0bd75453c8b07186bdd2ba
goodeffort <lukehsurvey@gmail.com>
2024-01-05 14:47:23 -0500

    changed Inventory controls to just use the ItemDisplay subcomponent

--- src/components/InventoryControls.vue
+++ src/components/InventoryControls.vue
@@ -1,70 +1,10 @@
 <script setup lang="ts">
-import PlannerSection from './PlannerSection.vue';
-import { usePlannerStore } from '../store/planner-store';
-import { storeToRefs } from 'pinia';
-
-const { getItemImageLink, craftItem } = usePlannerStore();
-
-const { inventoryItems, inventory } = storeToRefs(usePlannerStore());
+import ItemsDisplay from './ItemsDisplay.vue';
 </script>
 
 <template>
-    <PlannerSection title="Current Inventory" local-storage-id="inventory-controls-collapsed">
-        <div class="container">
-            <div class="row">
-                <div class="col-2" v-for="item in inventoryItems">
-                    <div class="item-col">
-                        <div class="text-align-end">
-                            <img :src="getItemImageLink(item)" :alt="item.name" class="img-thumbnail item-image" :style="{ opacity: inventory[item.itemId] > 0 ? '100%' : '30%' }" />
-                            <div class="craft-button" v-if="item.buildingProductList.length > 0">
-                                <button class="btn btn-primary" @click="craftItem(item)">Craft</button>
-                            </div>
-                        </div>
-                        <div class="name">
-                            {{ item.name }}
-                        </div>
-                        <div class="count">
-                            <input type="number" class="form-control" min="0" v-model="inventory[item.itemId]" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </PlannerSection>
+    <ItemsDisplay
+        title="Inventory"
+        local-storage-id="inventory-collapsed"
+    />
 </template>
-
-<style scoped>
-.item-image {
-    width: 100%;
-    height: 5em;
-    object-fit: contain;
-}
-
-.item-col {
-    width: 100%;
-    background-color: rgb(197, 197, 197);
-    padding: 1em 0.5em;
-    margin-bottom: 1em;
-    border-radius: 5px;
-}
-
-.name {
-    font-size: 0.8em;
-    text-align: center;
-    margin-top: 0.5em;
-    height: 4em;
-    text-align: center;
-    vertical-align: middle;
-}
-
-.craft-button {
-    position: relative;
-    top: -2.5em;
-    right: 2px;
-    height: 0px;
-}
-
-.text-align-end {
-    text-align: end;
-}
-</style>
