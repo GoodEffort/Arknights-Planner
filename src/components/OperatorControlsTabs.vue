09b70b18c0e97787cb94d1a357c075ce437f6315
goodeffort <lukehsurvey@gmail.com>
2024-07-16 15:19:22 -0400

    redesigned tabs for mobile

--- src/components/OperatorControlsTabs.vue
+++ src/components/OperatorControlsTabs.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-    defineEmits(['active', 'remove', 'plan', 'items']);
-    defineProps({
-        active: Boolean,
-        section: String
-    });
+defineEmits(['active', 'remove', 'plan', 'items']);
+defineProps({
+    active: Boolean,
+    section: String
+});
 </script>
 
 <template>
@@ -15,12 +15,36 @@
         <div class="col-8 text-center">
             <div class="btn-group" role="group" aria-label="Operator Controls">
                 <button type="button" class="btn btn-secondary" @click="$emit('plan')" :class="{ active: section === 'Plan' }
-                                        ">Plan</button>
-                <button type="button" class="btn btn-secondary" @click="$emit('items')" :class="{ active: section === 'Items' }">Items</button>
+                    ">Plan</button>
+                <button type="button" class="btn btn-secondary" @click="$emit('items')"
+                    :class="{ active: section === 'Items' }">Items</button>
             </div>
         </div>
         <div class="col-2 text-end">
             <button type="button" class="btn btn-danger" @click="$emit('remove')">Remove</button>
         </div>
     </div>
+    <div class="row d-flex d-md-none">
+        <div class="row">
+            <div class="btn-group" role="group">
+                <button type="button" class="btn" :class="`${active ? 'btn-success' : 'btn-secondary'}`"
+                    @click="$emit('active')">{{ active ? 'Active' : 'Inactive' }}</button>
+                <button type="button" class="btn btn-danger" @click="$emit('remove')">Remove</button>
+            </div>
+        </div>
+        <div class="row mt-1">
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-secondary" @click="$emit('plan')" :class="{ active: section === 'Plan' }
+                    ">Plan</button>
+                <button type="button" class="btn btn-secondary" @click="$emit('items')"
+                    :class="{ active: section === 'Items' }">Items</button>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+div.row.d-flex.d-md-none div.btn-group button.btn {
+    width: 50%;
+}
+</style>
