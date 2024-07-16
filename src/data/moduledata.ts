4dea5afbc47ff052801fe826ea999bd6bca58810
goodeffort <lukehsurvey@gmail.com>
2024-07-15 23:57:44 -0400

    fixed some moduless not showing up, cn_data was being overwritten that shouldn't have

--- src/data/moduledata.ts
+++ src/data/moduledata.ts
@@ -7,10 +7,9 @@ const getModuledata = async () => {
     const [response, cn_response] = await Promise.all([fetch(jsonLink), fetch(cn_jsonLink)]);
     const [data, cn_data]: UniEquip_Table[] = await Promise.all([response.json(), cn_response.json()]);
 
-    const combinedEquipDict = Object.assign(cn_data.equipDict, data.equipDict);
-    const combinedCharEquip = Object.assign(cn_data.charEquip, data.charEquip);
+    const combinedEquipDict = { ...cn_data.equipDict, ...data.equipDict };
 
-    return { ModuleDict: combinedEquipDict, CharacterModules: combinedCharEquip };
+    return { ModuleDict: combinedEquipDict, CharacterModules: cn_data.charEquip };
 };
 
 export default getModuledata;
