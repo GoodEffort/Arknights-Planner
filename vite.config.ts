460d1701604671a2a0c3d0e9f7bea8ef61a71bd2
goodeffort <lukehsurvey@gmail.com>
2024-08-05 18:14:02 -0400

    changed ssl plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), basicSsl()],
+  plugins: [vue(), mkcert()],
   base: '/Arknights-Planner/',
   define: {
     BUILD_DATE: JSON.stringify(new Date().toISOString())
