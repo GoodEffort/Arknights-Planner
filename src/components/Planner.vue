85669fa85846f127220e552e123bbe0a3be6d4fa
goodeffort <lukehsurvey@gmail.com>
2024-08-06 19:24:19 -0400

    removed spinner, added Emperor

--- src/components/Planner.vue
+++ src/components/Planner.vue
@@ -3,7 +3,6 @@ import AddOperators from './AddOperators.vue';
 import SelectedOperators from './SelectedOperators.vue';
 import { usePlannerStore } from '../store/planner-store';
 import { onMounted, ref } from 'vue';
-import { DotLoader } from "vue3-spinner";
 import InventoryControls from './InventoryControls.vue';
 import TotalCostOfPlan from './TotalCostOfPlan.vue';
 import MissingItems from './MissingItems.vue';
@@ -39,6 +38,48 @@ onMounted(async () => {
             <InventoryControls />
             <AddOperators />
         </div>
-        <DotLoader v-else />
+        <div v-else class="mt-5 spin-container">
+            <img src="/trap_035_emperor_1.png" class="loading-penguin" />
+        </div>
     </div>
 </template>
+
+<style scoped>
+.spin-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    transform: scale(1) rotate(0deg);
+    opacity: .8;
+	animation: pulse 2s infinite;
+
+}
+
+@keyframes pulse {
+	0% {
+		transform: scale(0.55) rotate(0deg);
+        opacity: .3;
+	}
+
+    25% {
+        transform: scale(1) rotate(5deg);
+        opacity: .8;
+    }
+    
+    50% {
+        transform: scale(1) rotate(-5deg);
+        opacity: .8;
+    }
+
+	70% {
+		transform: scale(1) rotate(0deg);
+        opacity: .8;
+	}
+
+	100% {
+		transform: scale(0.55) rotate(0deg);
+        opacity: 0.3;
+	}
+}
+</style>
