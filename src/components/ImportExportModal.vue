7ca2409ea0761b7beea566daee0ea359ab6ccd93
goodeffort <lukehsurvey@gmail.com>
2024-10-09 14:50:30 -0400

    Adding new import/export modal to put all Import/Export functionality in one place

---
+++ src/components/ImportExportModal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import Modal from './Modal.vue';
+import { usePlannerStore } from '../store/planner-store';
+import { setImportData } from '../store/store-operator-functions';
+
+const props = defineProps<{
+    modelValue: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: boolean): void;
+}>();
+
+const show = computed({
+    get: () => props.modelValue,
+    set: (value) => emit('update:modelValue', value)
+});
+</script>
+
+<template>
+  <modal v-model="show">
+    <template #header>
+      Import or Export Data
+    </template>
+    <template #body>
+    </template>
+    <template #footer>
+      <button class="btn btn-danger" @click="show = false">Cancel</button>
+    </template>
+  </modal>
+</template>
