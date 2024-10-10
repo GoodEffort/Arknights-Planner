8176090a3dfe87a7bc182948b8e05f5ec12b0c40
goodeffort <lukehsurvey@gmail.com>
2024-10-10 19:51:26 -0400

    simplified component

--- src/components/sections/MissingItems.vue
+++ src/components/sections/MissingItems.vue
@@ -2,15 +2,16 @@
 import { usePlannerStore } from '@/store/planner-store';
 import { storeToRefs } from 'pinia';
 import ItemsDisplay from '@/components/akplanner/ItemsDisplay.vue';
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 import PlannerSection from '@/components/akplanner/PlannerSection.vue';
 import { canCraft, getEXPValue, inventoryToList } from '@/store/store-inventory-functions';
 import ReservedItemsModal from '@/components/modals/ReservedItemsModal.vue';
 import ImportExportModal from '@/components/modals/ImportExportModal.vue';
 import { getMissingItems, getNeededEXPItems, getNeededItems } from '@/store/store-item-functions.';
+import NavTabList from '../generic/NavTabList.vue';
 
 const {
-    totalCosts, 
+    totalCosts,
     inventory,
     items,
     battleRecords,
@@ -68,26 +69,16 @@ const neededItems = computed(() => [
     ...neededEXPItems.value,
 ].sort((a, b) => a.item.sortId - b.item.sortId));
 
-type Tab = 'missing' | 'breakdown' | 'total';
-
-const tab = ref<Tab>((localStorage.getItem('missingItemTab') as Tab | null | '') || 'missing');
-watch(tab, () => localStorage.setItem('missingItemTab', tab.value));
-
 const totalCostsArray = computed(() =>
     inventoryToList(totalCosts.value, items.value)
         .filter(cost => cost.count > 0)
         .sort((a, b) => a.item.sortId - b.item.sortId));
 
-const displayItems = computed(() => {
-    switch (tab.value) {
-        case 'missing':
-            return neededItems.value;
-        case 'breakdown':
-            return itemsToFarm.value;
-        case 'total':
-            return totalCostsArray.value;
-    }
-});
+const tabs = [
+    { title: 'Total Cost', name: 'total' },
+    { title: 'Missing Items', name: 'missing' },
+    { title: 'Items to Farm/Craft', name: 'breakdown' }
+];
 
 const showReservedItemsModal = ref(false);
 const showPenguinExportModal = ref(false);
@@ -95,33 +86,32 @@ const showPenguinExportModal = ref(false);
 
 <template>
     <PlannerSection title="Missing Items and Recommendations" local-storage-id="needed-items-collapsed">
-        <ul class="nav nav-pills nav-fill">
-            <li class="nav-item" @click="tab = 'total'">
-                <a class="nav-link" :class="{ 'active': tab === 'total' }">Total Cost</a>
-            </li>
-            <li class="nav-item" @click="tab = 'missing'">
-                <a class="nav-link" :class="{ 'active': tab === 'missing' }">Missing Items</a>
-            </li>
-            <li class="nav-item" @click="tab = 'breakdown'">
-                <a class="nav-link" :class="{ 'active': tab === 'breakdown' }">Items to Farm/Craft</a>
-            </li>
-        </ul>
-        <hr />
-        <h3 v-if="tab === 'breakdown'" class="mb-4">Items To Farm</h3>
-        <button v-if="tab === 'breakdown'" @click="showPenguinExportModal = true" class="mb-4">Penguin Stats Farming
-            Plan Import</button>
-        <ItemsDisplay :display-items="displayItems" :farming="tab === 'breakdown' || tab === 'missing'"
-            :flash="tab === 'breakdown'" />
-        <hr v-if="tab === 'breakdown'" />
-        <h3 v-if="tab === 'breakdown'" class="mb-3">Items To Craft</h3>
-        <div v-if="tab === 'breakdown'" class="mb-4">
-            <button @click="showReservedItemsModal = true">Edit Reserved Items</button>
-        </div>
-        <ItemsDisplay v-if="tab === 'breakdown'" :display-items="itemsToCraft" :flash="tab === 'breakdown'" />
-        <div v-if="tab === 'breakdown'">
-            If using the +1/-1 buttons in the crafting section it will remove from the "Items to Farm" section above
-            first.
-        </div>
+        <NavTabList :tabs="tabs" selected="total">
+            <template #total>
+                <hr />
+                <ItemsDisplay :display-items="totalCostsArray" :farming="false" :flash="false" />
+            </template>
+            <template #missing>
+                <hr />
+                <ItemsDisplay :display-items="neededItems" :farming="true" :flash="false" />
+            </template>
+            <template #breakdown>
+                <hr />
+                <h3 class="mb-4">Items To Farm</h3>
+                <button @click="showPenguinExportModal = true" class="mb-4">Penguin Stats Farming Plan Import</button>
+                <ItemsDisplay :display-items="itemsToFarm" :farming="true" :flash="true" />
+                <hr />
+                <h3 class="mb-3">Items To Craft</h3>
+                <div class="mb-4">
+                    <button @click="showReservedItemsModal = true">Edit Reserved Items</button>
+                </div>
+                <ItemsDisplay :display-items="itemsToCraft" :flash="true" />
+                <div>
+                    If using the +1/-1 buttons in the crafting section it will remove from the "Items to Farm" section
+                    above first.
+                </div>
+            </template>
+        </NavTabList>
         <ReservedItemsModal v-model="showReservedItemsModal" />
         <ImportExportModal default-tab="penguin-stats" v-model="showPenguinExportModal" />
     </PlannerSection>
